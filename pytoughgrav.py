f9f68939c7425f61882181d699be5a5cd3265363
glbjch <brioch.hemmings@bristol.ac.uk>
2015-04-03 16:49:31 +0100

    quick change to ptg to give atmos a pmx equal to perm

--- pytoughgrav.py
+++ pytoughgrav.py
@@ -284,7 +284,7 @@ def grid2D(modelname,geo,dat,rocks,boundcol,lpregion=[[0,0,0],[0,0,0]],satelev=0
                 rockandincon(blk,grid,dat,None,initP,initSG,initT,pmx)
         else:
             rocktype='atmos'
-            pmx=None           
+            pmx=grid.rocktype[rocktype].permeability[0]          
             initP=atmosP
             initSG=0.99 # initial gas saturation  
             initT=25.0 # initial temperature - TOUGH2 doesn't seem to like < 1.0 C
