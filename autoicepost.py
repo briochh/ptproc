afff4dc092de12f637040c305a97f67da9d893c1
Brioch Hemmings <brioch.hemmings@bristol.ac.uk>
2015-06-14 10:34:05 +0100

    More work on automatic runthrough scripts

---
+++ autoicepost.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 05 17:48:26 2015
+
+@author: glbjch
+"""
+
+from t2data import *
+from t2grids import *
+from t2listing import *
+import os
+import pytoughgrav as ptg
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import ice_pytough as ipt
+import argparse
+
+#%% set up
+t0=tinit=time.clock()
+plt.close('all')
+parser = argparse.ArgumentParser(description='Prepare perturbation model')
+parser.add_argument('-b','--base', help='basemodel name',required=True)
+parser.add_argument('-l','--location', help='location',required=False, default='.')
+
+args = parser.parse_args()
+
+os.chdir(args.location)
+
+
+mod=args.base # define model name
+read=True ########### I N P U T #########################
+readgeo=True ########### I N P U T #########################
+geo_fname='grd.dat'
+readdat=True ########### I N P U T #########################
+dat_fname='flow2.inp'
+readresults=True ########### I N P U T #########################
+results_fname='flow2.out'
+readflow=True ########### I N P U T #########################
+#flowH_fname='results/FLOH.pkl'
+#readflowF=True ########### I N P U T #########################
+#flowF_fname='results/FLOLIQ.pkl'
+
+save=True ########### I N P U T #########################
+savevtk=True ########### I N P U T #########################
+flows={'FLOH':{},'FLO(LIQ.)':{},'FLO(GAS)':{}}
+
+print 'model=',mod
+#os.chdir('C:/Users/glbjch/Local Documents/Work/Modelling/Cotapaxi/'+mod)    
+if not os.path.exists('results'): 
+    os.makedirs('results')   
+    
+if read:
+    if readgeo is True: 
+        print 'Reading geometry from '+ geo_fname
+        geo=mulgrid(geo_fname)
+        geo.radial=False # set flag that geometry is not yet radial.
+    if readdat is True: 
+        print 'Reading input data from '+ dat_fname
+        dat=t2data(dat_fname) 
+    if readresults is True:
+        print 'Reading results from '+ results_fname
+        results=t2listing(results_fname)
+    if readflow is True:
+        if os.path.isfile('results/time.pkl'):
+            times=ptg.load_obj('results/time.pkl')
+        else: 
+        	print('CANT READ time FILE ......Continuing with times={}')
+        	times={}
+        for flow in flows.keys():
+            flow_fname='results/'+flow+'.pkl'
+            if os.path.isfile(flow_fname):
+                print 'Reading saturation data from '+ flow_fname
+                flows[flow]=ptg.load_obj(flow_fname)
+            else: 
+                print('CANT READ flow FILE ('+flow_fname+'......Continuing with flows['+flow+']={}')
+                flows[flow]={}
+    else: 
+        print('Not reading '+flow+'.pkl.flows['+flow+']={}')
+        flows[flow]={}
+        
+
+t1=time.clock()        
+print 'time to read=',(t1-t0)  
+      
+ipt.icepost(mod, geom_data=geo,tough2_input=dat, results=results, times=times, save=save, savevtk=savevtk,flows=flows)
+print 'time to run =', time.clock()-tinit
--- autoicertn.py
+++ autoicertn.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jun 12 14:43:04 2015
+Created on Tue Jun 09 15:41:29 2015
 
 @author: glbjch
 """
@@ -20,6 +20,7 @@ import argparse
 t0=time.clock()
 plt.close('all')
 #%%
+#os.chdir('C:/Users/glbjch/Local Documents/Work/Modelling/Cotapaxi')
 
 parser = argparse.ArgumentParser(description='Prepare perturbation model')
 parser.add_argument('-b','--base', help='basemodel name',required=True)
@@ -29,9 +30,9 @@ args = parser.parse_args()
 
 os.chdir(args.location)
 
-
-basemod=args.base
-mod=basemod+'_ptb'
+basemod=args.base+'_ptb'
+#basemod='Cota20150612_1_ptb'
+mod=args.base+'_rtn'
 if not os.path.exists(mod):
     os.makedirs(mod)
     
@@ -47,17 +48,15 @@ yrsec=365.25*3600*24
 dat.incon.clear()
 inc=t2incon('flow2.sav')
 
-dat.parameter['option'][12]= 0
-dat.parameter['timestep']=[1.0, 1.0E3,8.6400e+04]
-dat.output_times['time']=[1.0]
-dat.output_times['time_increment']= 10*yrsec
-
-dat.output_times['num_times_specified']=1
-dat.output_times['num_times']=200
-dat.parameter['max_timestep']=10*yrsec # maximum timstep length
-dat.parameter['print_interval']=50 # print (output) frequency to flow.out
-dat.parameter['tstop']=1E3*yrsec
-
+# additional output parameters 
+dat.parameter['max_timestep']=3.1558e+09 # maximum timstep length
+dat.parameter['print_interval']=20 # print (output) frequency to flow.out
+dat.parameter['timestep']=[1.0E3,8.6400e+04,3.1558e+08] # initial timestep?
+dat.parameter['tstop']=None
+dat.output_times['time']=[1000.0,3600.0,8.6400e+04,3.1558e+07,3.1558e+08,3.1558e+09,3.1558e+10] # predefined output times
+dat.output_times['num_times_specified']=len(dat.output_times['time'])
+dat.output_times['num_times']=150
+dat.output_times['time_increment']= 100*yrsec
 
 for col in geo.columnlist[:]:
 #        if col not in ecol:
@@ -65,11 +64,8 @@ for col in geo.columnlist[:]:
     blkname=geo.block_name(lay.name,col.name) # get block name for the bottom layer of this column
     x=grid.block[blkname].centre[0]
     if x <= 500:
-        cond=inc[blkname]
         print blkname
-        initT=350
-        cond.variable[2]=initT
-        grid.block[blkname].volume=grid.block[blkname].volume*1E50
+        grid.block[blkname].volume=grid.block[blkname].volume/1E50
 
 geo.write(mod + '/grd.dat')
 grid.write_vtk(geo,mod+'/'+mod+'_initial.vtk') 
@@ -77,7 +73,5 @@ dat.write(mod + '/flow2.inp')
 
 inc.write(mod + "/flow2.inc")
 
-
 shutil.copy(basemod,mod+'/'+mod)
 shutil.copy('genertot.txt',mod+'/')
-
