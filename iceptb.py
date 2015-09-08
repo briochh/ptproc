2bcf1cea2a441902cb97cbab0031ec8520ac3d64
glbjch <brioch.hemmings@bristol.ac.uk>
2015-09-08 08:40:15 +0100

    20150815_1 rainfall ptb

--- iceptb.py
+++ iceptb.py
@@ -23,7 +23,7 @@ os.chdir('C:/Users/glbjch/Local Documents/Work/Modelling/Cotapaxi')
 
 
 basemod='Cota20150805_1'
-mod=basemod+'_ptb1'
+mod=basemod+'_ptb2'
 if not os.path.exists(mod):
     os.makedirs(mod)
     
@@ -31,7 +31,7 @@ dat=t2data(basemod+'/flow2.inp')
 geo=mulgrid(basemod+'/grd.dat')
 grid=dat.grid
 width=geo.bounds[1][1]-geo.bounds[0][1]
-#ptg.makeradial(geo,None,width) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ptg.makeradial(geo,None,width) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 yrsec=365.25*3600*24    
 # INCON
@@ -54,16 +54,17 @@ dat.parameter['tstop']=5E3*yrsec
 main=grid.rocktype['main ']
 main.permeability=main.permeability*10
 
-for blk in grid.blocklist:
-    if blk.rocktype.name == 'main ':
+#for blk in grid.blocklist:
+#    if blk.rocktype.name == 'main ':
+#        #blk.pmx
+#        blk.pmx=blk.pmx*10
         #blk.pmx
-        blk.pmx=blk.pmx*10
-        #blk.pmx
-#dat.clear_generators()
-#heat_flux=0.24
-#for blk in grid.blocklist[0:]: blk.hotcell=False
-#ipt.heatgen(mod,geo,dat,grid,heat_flux,function={'type':'log','points':[[5.0,2.],[10000.,0.24]]},inject=[150,2.0e-3,1.67e6])
-#ptg.gen_constant(mod,geo,grid,dat,constant=1.5e-5,enthalpy='var',cfix=[350,80.0])#enthalpy=8440.)
+
+dat.clear_generators()
+heat_flux=0.24
+for blk in grid.blocklist[0:]: blk.hotcell=False
+ipt.heatgen(mod,geo,dat,grid,heat_flux,function={'type':'log','points':[[5.0,2.],[10000.,0.24]]},inject=[150,1e-3,1.67e6])
+ptg.gen_constant(mod,geo,grid,dat,constant=3e-5,enthalpy='var')#enthalpy=8440.)
 
 
 #for col in geo.columnlist[:]:
